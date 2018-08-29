892bd2a97d0ca810d5f6c84b7370db5ecab787e8
Kacper Wikieł <random8902@gmail.com>
2018-08-29 10:41:59 +0200

    Make index the homepage

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root 'welcome#index'
+  root 'articles#index'
   resources :articles
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
